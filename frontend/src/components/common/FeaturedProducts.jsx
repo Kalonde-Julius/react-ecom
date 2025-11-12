c614149829022a52ed4a6271c476214ff5fcbf45
Kalonde-Julius <Kalondejulius@gmail.com>
2025-11-13 01:18:25 +0300

    1st commit

---
+++ frontend/src/components/common/FeaturedProducts.jsx
@@ -0,0 +1,156 @@
+import React from 'react'
+import product2 from '../../assets/images/men/men-2.avif';
+
+const FeaturedProducts = () => {
+
+  return (
+
+    <section className='section-2 py-1'>
+    <div className='container'>
+    <h2> Featured Products </h2>
+
+    <div className='row mt-4'>
+
+    <div className='col-md-3'>
+        <div className='product card border-0'>
+            <div className='card-img'>
+                <img src={product2} alt="product2" className='w-100'/>
+            </div>
+            <div className='card-body pt-3 mx-5'>
+                <a href='#'> Casual Shirt </a>
+                <div className='price'> 
+                    UGX 40,000/=  <br/>
+                    <span className='text-decoration-line-through'> 
+                        UGX 60,000/= 
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className='col-md-3'>
+        <div className='product card border-0'>
+            <div className='card-img'>
+                <img src={product2} alt="product2" className='w-100'/>
+            </div>
+            <div className='card-body pt-3 mx-5'>
+                <a href='#'> Casual Shirt </a>
+                <div className='price'> 
+                    UGX 40,000/=  <br/>
+                    <span className='text-decoration-line-through'> 
+                        UGX 60,000/= 
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className='col-md-3'>
+        <div className='product card border-0'>
+            <div className='card-img'>
+                <img src={product2} alt="product2" className='w-100'/>
+            </div>
+            <div className='card-body pt-3 mx-5'>
+                <a href='#'> Casual Shirt </a>
+                <div className='price'> 
+                    UGX 40,000/=  <br/>
+                    <span className='text-decoration-line-through'> 
+                        UGX 60,000/= 
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className='col-md-3'>
+        <div className='product card border-0'>
+            <div className='card-img'>
+                <img src={product2} alt="product2" className='w-100'/>
+            </div>
+            <div className='card-body pt-3 mx-5'>
+                <a href='#'> Casual Shirt </a>
+                <div className='price'> 
+                    UGX 40,000/=  <br/>
+                    <span className='text-decoration-line-through'> 
+                        UGX 60,000/= 
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className='col-md-3'>
+        <div className='product card border-0'>
+            <div className='card-img'>
+                <img src={product2} alt="product2" className='w-100'/>
+            </div>
+            <div className='card-body pt-3 mx-5'>
+                <a href='#'> Casual Shirt </a>
+                <div className='price'> 
+                    UGX 40,000/=  <br/>
+                    <span className='text-decoration-line-through'> 
+                        UGX 60,000/= 
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className='col-md-3'>
+        <div className='product card border-0'>
+            <div className='card-img'>
+                <img src={product2} alt="product2" className='w-100'/>
+            </div>
+            <div className='card-body pt-3 mx-5'>
+                <a href='#'> Casual Shirt </a>
+                <div className='price'> 
+                    UGX 40,000/=  <br/>
+                    <span className='text-decoration-line-through'> 
+                        UGX 60,000/= 
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className='col-md-3'>
+        <div className='product card border-0'>
+            <div className='card-img'>
+                <img src={product2} alt="product2" className='w-100'/>
+            </div>
+            <div className='card-body pt-3 mx-5'>
+                <a href='#'> Casual Shirt </a>
+                <div className='price'> 
+                    UGX 40,000/=  <br/>
+                    <span className='text-decoration-line-through'> 
+                        UGX 60,000/= 
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className='col-md-3'>
+        <div className='product card border-0'>
+            <div className='card-img'>
+                <img src={product2} alt="product2" className='w-100'/>
+            </div>
+            <div className='card-body pt-3 mx-5'>
+                <a href='#'> Casual Shirt </a>
+                <div className='price'> 
+                    UGX 40,000/=  <br/>
+                    <span className='text-decoration-line-through'> 
+                        UGX 60,000/= 
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+    </div>
+    </section>
+  )
+}
+
+export default FeaturedProducts
---
+++ frontend/src/components/common/LatestProducts.jsx
@@ -0,0 +1,153 @@
+import React from 'react'
+import product1 from '../../assets/images/men/men-1.avif';
+
+const LatestProducts = () => {
+  return (
+    <section className='section-2 py-2'>
+        <div className='container'>
+            <h2> New Arrivals </h2>
+
+            <div className='row mt-4'>
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'>
+                            <img src={product1} alt="product1" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3 mx-5'>
+                            <a href='#'> Casual Shirt </a>
+                            <div className='price'> 
+                                UGX 40,000/=  <br/>
+                                <span className='text-decoration-line-through'> 
+                                    UGX 60,000/= 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'>
+                            <img src={product1} alt="product1" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3 mx-5'>
+                            <a href='#'> Casual Shirt </a>
+                            <div className='price'> 
+                                UGX 40,000/=  <br/>
+                                <span className='text-decoration-line-through'> 
+                                    UGX 60,000/= 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'>
+                            <img src={product1} alt="product1" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3 mx-5'>
+                            <a href='#'> Casual Shirt </a>
+                            <div className='price'> 
+                                UGX 40,000/=  <br/>
+                                <span className='text-decoration-line-through'> 
+                                    UGX 60,000/= 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'>
+                            <img src={product1} alt="product1" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3 mx-5'>
+                            <a href='#'> Casual Shirt </a>
+                            <div className='price'> 
+                                UGX 40,000/=  <br/>
+                                <span className='text-decoration-line-through'> 
+                                    UGX 60,000/= 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'>
+                            <img src={product1} alt="product1" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3 mx-5'>
+                            <a href='#'> Casual Shirt </a>
+                            <div className='price'> 
+                                UGX 40,000/=  <br/>
+                                <span className='text-decoration-line-through'> 
+                                    UGX 60,000/= 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'>
+                            <img src={product1} alt="product1" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3 mx-5'>
+                            <a href='#'> Casual Shirt </a>
+                            <div className='price'> 
+                                UGX 40,000/=  <br/>
+                                <span className='text-decoration-line-through'> 
+                                    UGX 60,000/= 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'>
+                            <img src={product1} alt="product1" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3 mx-5'>
+                            <a href='#'> Casual Shirt </a>
+                            <div className='price'> 
+                                UGX 40,000/=  <br/>
+                                <span className='text-decoration-line-through'> 
+                                    UGX 60,000/= 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'>
+                            <img src={product1} alt="product1" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3 mx-5'>
+                            <a href='#'> Casual Shirt </a>
+                            <div className='price'> 
+                                UGX 40,000/=  <br/>
+                                <span className='text-decoration-line-through'> 
+                                    UGX 60,000/= 
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default LatestProducts
